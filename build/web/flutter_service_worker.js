d883d7f88ecdc64fadf7998da45424cbe5545db8
oishii-pg-gohan <oishii.pg.gohan@gmail.com>
2023-01-27 06:31:59 +0900

    チャンタの名称を混全帯么九に変更

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -24,9 +24,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1def0c6ba2a31adee5848c2c97d68230",
-"/": "1def0c6ba2a31adee5848c2c97d68230",
-"main.dart.js": "2df979e1d174da0c7a961d705d80a230",
+"index.html": "acaba7885304656de80c033e7b857b94",
+"/": "acaba7885304656de80c033e7b857b94",
+"main.dart.js": "c3e4497a86ba742485b4627ac57f452f",
 "manifest.json": "706c847a3e30e81fdbeb5dc1fc62a750",
 "version.json": "9c657deafc0fc254632d3a8b63ce5e22"
 };
